74a6a1ac24c87f1b2e555a9022b63c0194731dbb
Awesozach78 <20171340@archwood.org>
2017-01-31 09:55:48 -0500

    Ying

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+/**
+ * Created by 20171340 on 1/31/2017.
+ */
+import java.io.*;
+public class Student {
+
+}
+
+
+
+
+//https://www.tutorialspoint.com/java/java_files_io.htm
+//http://home.wlu.edu/~lambertk/hsjava/whatsnew/input.html
